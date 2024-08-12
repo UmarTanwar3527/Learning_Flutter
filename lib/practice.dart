14e16ce061a84c7607858fad39018e188df5d6ca
umartanwar11651913 <umartanwar11651913@gmail.com>
2024-08-12 06:45:38 +0000

    some program changes

--- lib/practice.dart
+++ lib/practice.dart
@@ -8,10 +8,10 @@ void main() {
 
   // stdout.write("Welcome $name ");
 
-  for (final char in 'hii umar tanwar'.split('')) {
-    // print(char); // print() adds a newline
-    stdout.write('$char\n'); // stdout.write() does not add a newline
-  }
+  // for (final char in 'hii umar tanwar'.split('')) {
+  //   // print(char); // print() adds a newline
+  //   stdout.write('$char\n'); // stdout.write() does not add a newline
+  // }
 
   Human(); // obejct
 }
