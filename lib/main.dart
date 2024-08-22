6a5eb5191aa4cbbcd3e3cb615b5b3ee8f8bcd9fd
umartanwar11651913 <umartanwar11651913@gmail.com>
2024-08-22 08:49:32 +0000

    Understainding How to Add Decoration to Container

--- lib/main.dart
+++ lib/main.dart
@@ -360,10 +360,30 @@ class _MyHomePageState extends State<MyHomePage> {
       //   // scrollDirection: Axis.horizontal,
       // ),
 
+      // Understainding How to Add Decoration to Container
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        color: Colors.blue.shade50,
+        // decoration: ,
+        child: Center(
+          child: Container(
+            width: 150,
+            height: 150,
+            decoration: BoxDecoration(
+              color: Colors.deepPurple,
+              // border: Border.all(color: Colors.black, width: 2),
+              // borderRadius: BorderRadius.circular(15),
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(21),
+                bottomRight: Radius.circular(21),
+              ),
+            ),
+          ),
+        ),
+      ),
 
-      // 
-
-
+      
 
       // UnderstaindingGridView Widget
       // body: GridView.count(
